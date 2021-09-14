fad163e217038d01d22a994bea51c9d3aef741a7
Jakub Molicki <molickikuba@gmail.com>
2021-09-14 17:51:22 +0200

    lesson6

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,24 @@
-const getTodos = (callback) => {
+const getTodos = (resourse, callback) => {
     const request = new XMLHttpRequest();
     // sledzimy postep
     request.addEventListener('readystatechange', ()=> {
         if(request.readyState === 4 && request.status === 200){
             const data = JSON.parse(request.responseText)
             callback(undefined, data);
-            data.forEach(element => {
-                console.log(element)
-            });
         } else if(request.readyState === 4){ // stan 4 odpowiada za zakonczoną operacje
             callback('could not fetch data', undefined);
         }
     })
-    request.open('GET', 'todos.json'); // okreslamy co i gdzie chcemy zrobic
+    request.open('GET', resourse); // okreslamy co i gdzie chcemy zrobic
     request.send(); // wysylamy żądanie
 }
-
-console.log(1);
-console.log(2);
-getTodos((err, data) => {
-    if(err){
-        console.log(err)
-    } else {
-        console.log(data)
-    }
+// CALLBACK HELL
+getTodos('./todos/luigi.json', (err, data) => {
+    console.log("Luigi", data)
+    getTodos('./todos/mario.json', (err, data) => {
+        console.log("Mario", data)
+        getTodos('./todos/shaun.json', (err, data) => {
+            console.log("Shaun", data)
+        })
+    })
 });
-console.log(3);
-console.log(4);
