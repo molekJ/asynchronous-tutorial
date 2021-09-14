fbbc380b7c038f270dd4845c5f3dcda3d657b081
Jakub Molicki <molickikuba@gmail.com>
2021-09-14 18:12:25 +0200

    lesson7

--- sandbox.js
+++ sandbox.js
@@ -1,24 +1,23 @@
-const getTodos = (resourse, callback) => {
-    const request = new XMLHttpRequest();
-    // sledzimy postep
-    request.addEventListener('readystatechange', ()=> {
-        if(request.readyState === 4 && request.status === 200){
-            const data = JSON.parse(request.responseText)
-            callback(undefined, data);
-        } else if(request.readyState === 4){ // stan 4 odpowiada za zakonczoną operacje
-            callback('could not fetch data', undefined);
-        }
-    })
-    request.open('GET', resourse); // okreslamy co i gdzie chcemy zrobic
-    request.send(); // wysylamy żądanie
-}
-// CALLBACK HELL
-getTodos('./todos/luigi.json', (err, data) => {
-    console.log("Luigi", data)
-    getTodos('./todos/mario.json', (err, data) => {
-        console.log("Mario", data)
-        getTodos('./todos/shaun.json', (err, data) => {
-            console.log("Shaun", data)
+const getTodos = (resourse) => {
+
+    return new Promise ((resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        request.addEventListener('readystatechange', ()=> {
+            if(request.readyState === 4 && request.status === 200){
+                const data = JSON.parse(request.responseText)
+                resolve(data);
+            } else if(request.readyState === 4){ 
+                reject('Error gettin resource')
+            }
         })
+        request.open('GET', resourse); 
+        request.send();
     })
-});
+}
+
+getTodos('./todos/luigi.json').then(data=> {
+    console.log("udało się pobrac dane:", data)
+}).catch(err=> {
+    console.log("Nie udało się pobrać danych",err)
+})
