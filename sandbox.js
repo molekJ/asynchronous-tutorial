cab4f7e35c2e15595bac540e917c3e174b5158c8
Jakub Molicki <molickikuba@gmail.com>
2021-09-15 13:39:20 +0200

    lesson10

--- sandbox.js
+++ sandbox.js
@@ -1,20 +1,8 @@
-// fetch
+const getTodos = async () => {
+  const response = await fetch("./todos/luigi.json");
+  // gdy obietnica w fetch się rozwiąże, możemy pobrać dane i przypisać je. obietnica zwraca promise, dlatego nie musimy uzywać .then()
+  const data = await response.json(); // zwracamy promis, gdy proimis sie rozwiaze- przypiszemy wartosc do data
+  return data;
+};
 
-fetch("./todos/luigi.json") // przekazujemy argument będący zasobem
-  .then((response) => {
-    // obsługa po sukcesie // resolve
-    console.log("suksec:", response);
-    return response.json(); //ta metoda daje nam dane
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    // obsługa po niepowodzeniu, uruchamia funkcje zwrotną // reject
-    console.log("err`:", err);
-  });
-// obietnica jest odrzucana tylko wtedy, gdy pojawia się jakiś błąd sieci
-// błędny adres URL lub punkt końcowy lub zasób nie wywali błędu
-// 1. pobieramy dane => FETCH
-// 2. bierzemy odpowiedź i zwracamy odpowiedź.json, który zwraca obietnicę
-// 3. uruchamiamy funkcję w której faktycznie mamy dostęp do danych
+getTodos().then((data) => console.log("ready", data));
