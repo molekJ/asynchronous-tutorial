8980a8549c4267ce7da52bdf6dd4da83302e34d9
Jakub Molicki <molickikuba@gmail.com>
2021-09-15 13:56:41 +0200

    lesson11

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,17 @@
 const getTodos = async () => {
-  const response = await fetch("./todos/luigi.json");
-  // gdy obietnica w fetch się rozwiąże, możemy pobrać dane i przypisać je. obietnica zwraca promise, dlatego nie musimy uzywać .then()
-  const data = await response.json(); // zwracamy promis, gdy proimis sie rozwiaze- przypiszemy wartosc do data
+  const response = await fetch("./todods/luigi.json");
+  // jezeli bedziemy mieli blad URL czy zasobie będzie to jako błąd w pliku JSON co jest nieprawdą. Fetch się rozwiązuje, gdy próbujemy użyć .json() który nie może przekształcić danych - po nie zostały znalezione.
+  // dlatego musimy ręcznie sprawdzić czy nie ma statusu 200
+
+  if (response.status !== 200) {
+    throw new Error("Problem z pobraniem danych"); // wyrzucamy własny błąd, co zgłosi nowt błąd
+  } // a kiedy zgłaszamy nowy blad wewnatrz funckcji async
+  // obietnica zwrócona przez tą funkcję jest odrzucona
+  // dalszy kod się nie wykona
+  const data = await response.json();
   return data;
 };
 
-getTodos().then((data) => console.log("ready", data));
+getTodos()
+  .then((data) => console.log("ready", data))
+  .catch((err) => console.log("rejected", err.message));
